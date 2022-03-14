1e13f45edeb9f010c8c587844e93a937be8dd09e
William Clemmons Jr <wacboss@live.com>
2022-03-13 23:47:18 -0500

    working on selecting the right types of cars before searching

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 def create_webdriver():
     driver = webdriver.Chrome()
@@ -14,8 +15,14 @@ def set_years(driver):
     year1 = Select(driver.find_element_by_id("year1"))
     year1.select_by_value("1984")
 
-# Open Webdriver
+
 driver = create_webdriver()
 set_years(driver)
+driver.find_element(By.LINK_TEXT, "Vehicle Type").click()
+time.sleep(3)
+#TODO click checkbox for plug-in hybrids and all electrics
+# just need to scroll to the element then click it (actions)
+driver.find_element(By.NAME, "cbvtplugin").click()
+#driver.find_element_by_id("cbvtElectric").click()
 time.sleep(1000)
 driver.close()
