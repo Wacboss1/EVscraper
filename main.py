031a29ea26f47591d8f154f4f13dcab64d6d467f
William Clemmons Jr <wacboss@live.com>
2022-03-13 22:49:16 -0500

    Gets all EVs from the 1984 to the current year

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import time
+
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+def create_webdriver():
+    driver = webdriver.Chrome()
+    driver.get("https://www.fueleconomy.gov/feg/powerSearch.jsp")
+    return driver
+
+def set_years(driver):
+    year1 = Select(driver.find_element_by_id("year1"))
+    year1.select_by_value("1984")
 
 # Open Webdriver
-driver = webdriver.Chrome()
-driver.get("https://www.fueleconomy.gov/feg/powerSearch.jsp")
-
-
+driver = create_webdriver()
+set_years(driver)
+time.sleep(1000)
 driver.close()
