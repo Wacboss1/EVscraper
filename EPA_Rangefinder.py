0b7375e64fb276bdacc2cdf94754aaa756d0b90e
William Clemmons <wacboss@live.com>
2022-03-15 13:55:44 -0500

    sucessfully scrapes info from the first page but errors before starting the next one.

--- EPA_Rangefinder.py
+++ EPA_Rangefinder.py
@@ -1,4 +1,5 @@
 import time
+import re
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -55,12 +56,39 @@ def click_search_button():
 driver = create_webdriver()
 search_bev()
 
-# TODO extract info from each entry based on if it is a EV
+# TODO extract Name,City efficiency, highway efficiency, combined efficiency, and total Range from each entry
+
+
+
+def go_to_car_info(car):
+    car_link = car.find_element(By.TAG_NAME, "a")
+    move_to(car_link, click=True)
+
+
+def get_car_name():
+    name_element = soup.find("th", class_="sbsCellHeader")
+    return " ".join(name_element.get_text().split()[1:])  # Removes "X" and whitespace
+
+
+def extract_fuel_economy():
+    digits = re.compile("\\d+")
+    combined_fuel_economy = re.search(digits, soup.find("td", class_="combinedMPG").get_text()).group(0)
+    city_fuel_economy = re.search(digits, soup.find_all("td", class_="ctyhwy")[0].get_text()).group(0)
+    highway_fuel_economy = re.search(digits, soup.find_all("td", class_="ctyhwy")[1].get_text()).group(0)
+    kwh_per_100miles = re.search(digits, soup.find("td", class_="fuelconsumption").get_text()).group(0)
+    total_range = re.search(digits, soup.find("div", class_="totalRange").get_text()).group(0)
+    return total_range, combined_fuel_economy, city_fuel_economy, highway_fuel_economy, kwh_per_100miles
+
+
 list_of_cars = driver.find_element(By.CLASS_NAME, "cars")
 for car in list_of_cars.find_elements(By.CLASS_NAME, "ymm-row"):
-    car_link = car.find_element(By.TAG_NAME, "a")
-    car_link.click()
-
+    #TODO store the href for each element then scrape each link
+    go_to_car_info(car)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    name = get_car_name()
+    max_range, combined, city, highway, kwh_per_100mi = extract_fuel_economy()
+    driver.back()
+    time.sleep(5)
 
 time.sleep(1000)
 driver.close()
